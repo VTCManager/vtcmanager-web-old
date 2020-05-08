20fead7193206b4d63bc46fcf2373ff8fc93fd39
Joschua H <joschua.hass@t-online.de>
2020-05-08 13:39:10 +0200

    added email not verified warning

--- vtcmanager-web/app/Http/Controllers/HomeController.php
+++ vtcmanager-web/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,14 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
+        $user = Auth::user();
+        if(!$user->hasVerifiedEmail()){
+            if (!$request->session()->has('users')) {
+                $request->session()->put('warning', __('interface.email_not_verified'));
+            }
+        }
         return view('interface');
     }
 }
--- vtcmanager-web/resources/views/interface.blade.php
+++ vtcmanager-web/resources/views/interface.blade.php
@@ -13,6 +13,11 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    @if (session('warning'))
+                        <div class="alert alert-danger" role="alert">
+                            {{ session('warning') }}
+                        </div>
+                    @endif
 
                     You are logged in!
                 </div>
