df8eae622c807e22ca59c8188b6bab33926bbaf9
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-06-12 21:44:34 +0200

    removed routing and translation for old ClientKeys

--- vtcmanager-web/resources/lang/de/settings.php
+++ vtcmanager-web/resources/lang/de/settings.php
@@ -3,13 +3,4 @@
 return [
     /* CLIENT SETTINGS*/
     'client' => 'Client',
-    'client_api_key_name' => 'Name',
-    'client_api_key_created_at' => 'Erstellt am',
-    'client_api_key_key' => 'Zugangscode',
-    'client_api_key_ip' => 'Ausgestellt für IP-Adresse',
-    'create_client_key' => 'Zugangsschlüssel erstellen',
-    'delete' => 'Löschen',
-    /* CREATE KEY MODAL */
-    'create_client_key_ip_warning' => 'WARNUNG: Der Zugangsschlüssel wird auf deine aktuelle IP-Adresse ausgestellt. Dies dient zum Schutz deines Accounts.',
-    'create_client_key_your_ip' => 'Deine IP-Adresse',
 ];
--- vtcmanager-web/resources/views/interface/settings.blade.php
+++ vtcmanager-web/resources/views/interface/settings.blade.php
@@ -1,29 +1,6 @@
 @extends('layouts.interface')
 
 @section('content')
-<div class="modal fade" id="new-client-key" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">{{ __('settings.create_client_key') }}</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <form action="{{ route('create-client-key') }}" method="post">
-          @csrf
-        <div class="modal-body">
-            <p class="text-danger">{{ __('settings.create_client_key_ip_warning') }}</p>
-            <p>{{ __('settings.create_client_key_your_ip') }}: {{Request::ip()}}</p>
-            <input class="form-control text-white" name="description" maxlength="255" type="text" placeholder="Beschreibung">
-        </div>
-        <div class="modal-footer">
-          <button type="submit" class="btn btn-success">Save changes</button>
-        </div>
-      </form>
-      </div>
-    </div>
-  </div>
 <h1>Einstellungen</h1>
 <nav>
     <div class="nav nav-tabs" id="nav-tab" role="tablist">
@@ -35,7 +12,7 @@
   <div class="tab-content" id="nav-tabContent">
     <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
     <div class="tab-pane fade" id="client-page" role="tabpanel" aria-labelledby="client-tab">
-      
+
     </div>
     <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
   </div>
